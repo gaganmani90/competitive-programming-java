35de2d34d326e30d1fcada12d45c3a46bdbb3e57
Mani <gagamani@amazon.com>
2017-03-16 21:16:22 +0530

    Checking in few more files

--- src/hackerrank/contest/BigString.java
+++ src/hackerrank/contest/BigString.java
@@ -1,4 +1,4 @@
-package hackerrank;
+package hackerrank.contest;
 
 import java.util.Scanner;
 
--- src/hackerrank/contest/Megaprime.java
+++ src/hackerrank/contest/Megaprime.java
@@ -1,8 +1,14 @@
-package hackerrank.datastructure;
+package hackerrank.contest;
 
 import java.util.HashSet;
 import java.util.Scanner;
 
+/**
+ * 
+ * Not scalable, failing for big numbers.
+ * @author gagamani
+ *
+ */
 public class Megaprime {
 
 	private static HashSet<Long> primeSet = getPrimeSet();
@@ -15,25 +21,24 @@ public class Megaprime {
 		long first = in.nextLong();
 		long last = in.nextLong();
 
-		
-		
+
+
 		long t1 = System.currentTimeMillis()/1000;
-		//System.out.println(getMegaPrimeNumberCount(first, last));
+		System.out.println(getMegaPrimeNumberCount(first, last));
 		long t2 = System.currentTimeMillis()/1000;
-		//System.out.println((t2-t1));
-		//System.out.println(megaPrimeSet);
-		//System.out.println("======");
-		//System.out.println(primeSet);
-		System.out.println(isPrime(first));
+
+		System.out.println((t2-t1));
 	}
 
 	private static int getMegaPrimeNumberCount(long a, long b){
 		int count = 0;
-		
 		for(long i = a; i <= b; i++){
-			if(isMegaPrime(i)){
+			long t1 = System.currentTimeMillis();
+			if(isPrime(i) && isMegaPrime(i)){
 				count++;
 			}
+			long t2 = System.currentTimeMillis();
+			//System.out.println((t2-t1));
 		}
 
 		return count;
@@ -43,34 +48,30 @@ public class Megaprime {
 
 		long copyn = n;
 		boolean isMegaPrime = true;
-		
+
 		if(megaPrimeSet.contains(n) && n != 1){
 			return true;
 		}
 		else{
 			long rem, div;
-			if(primeSet.contains(n) || isPrime(n)){
-				while(n != 0){
-					rem = n%10;
-					n = n/10;
-				
-					if(!primeSet.contains(rem)){
-						isMegaPrime = false;
-						break;
-					}
-					else if(megaPrimeSet.contains(n)){
-						//System.out.println(n+":"+hit++);
-						isMegaPrime = true;
-						break;
-					}
+			while(n != 0){
+				rem = n%10;
+				n = n/10;
+
+				if(!primeSet.contains(rem)){
+					isMegaPrime = false;
+					break;
+				}
+				else if(megaPrimeSet.contains(n)){
+					//System.out.println(n+":"+hit++);
+					isMegaPrime = true;
+					break;
 				}
 			}
-			else{
-				return false;
-			}
 			if(isMegaPrime){
-				megaPrimeSet.add(copyn);
-				//System.out.println(copyn);
+				if(primeSet.contains(copyn) || isPrime(copyn)){
+					megaPrimeSet.add(copyn);
+				}
 			}
 
 		}
@@ -94,18 +95,18 @@ public class Megaprime {
 				return false;
 			}
 			else if ( num > 2 && num%2 == 0 ) {
-		        return false;
-		    }
-		    int top = (int)Math.sqrt(num) + 1;
-		    for(int i = 3; i < top; i+=2){
-		        if(num % i == 0){
-		            return false;
-		        }
-		    }
+				return false;
+			}
+			int top = (int)Math.sqrt(num) + 1;
+			for(int i = 3; i < top; i+=2){
+				if(num % i == 0){
+					return false;
+				}
+			}
 			primeSet.add(num);
 			return true;
 		}
-		
+
 	}
 
 }
