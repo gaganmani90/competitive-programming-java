2100a7470295a62558eed92e418cdd3ae2304aa5
Gagan Mani <gagamani@amazon.com>
2018-09-15 16:47:10 +0530

    air ticket, merge sorted arrays

--- src/main/java/programs/array/MergeSortedArrays.java
+++ src/main/java/programs/array/MergeSortedArrays.java
@@ -8,10 +8,27 @@ import java.util.Arrays;
  */
 public class MergeSortedArrays {
 
+    /**
+     * By using insertion sort
+     * @param arr1
+     * @param arr2
+     * @return
+     */
     static int[] mergeByInsertion(int[] arr1, int[] arr2){
         int[] sortedArray = new int[arr1.length+arr2.length];
 
         sortedArray = Arrays.copyOf(arr1, sortedArray.length);
+
+
+        for(int i = 0, j = arr1.length-1; i < arr2.length; i++,j++){
+            int key = arr2[i];
+            int tempIndex = j;
+            while (tempIndex >= 0 && sortedArray[tempIndex] > key){
+                sortedArray[tempIndex+1] = sortedArray[tempIndex];
+                tempIndex = tempIndex - 1;
+            }
+            sortedArray[tempIndex+1] = key;
+        }
         return sortedArray;
     }
 }
---
+++ src/test/java/programs/array/MergeSortedArraysTest.java
@@ -0,0 +1,19 @@
+package programs.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author gagamani
+ * @date 9/15/18
+ */
+public class MergeSortedArraysTest {
+
+    @Test
+    public void mergeByInsertion(){
+        Assert.assertEquals("[1, 2, 3, 4, 4, 5, 6, 8]", Arrays.toString(MergeSortedArrays.mergeByInsertion(new int[]{1,3,4,5}, new int[]{2,4,6,8})));
+        Assert.assertEquals("[0, 1, 3, 21]", Arrays.toString(MergeSortedArrays.mergeByInsertion(new int[]{1,3}, new int[]{21,0})));
+    }
+}
